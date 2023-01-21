e702197fbf3d385fae361ae36118cad9da9613d6
Matheus de Bortoli Pastega <matheuspastega@gmail.com>
2023-01-21 04:01:08 -0300

    Input subsystem

--- engine/src/core/application.c
+++ engine/src/core/application.c
@@ -4,6 +4,7 @@
 #include "logger.h"
 
 #include "core/event.h"
+#include "core/input.h"
 #include "core/kmemory.h"
 #include "platform/platform.h"
 
@@ -31,6 +32,7 @@ b8 application_create(game* game_inst)
 
     // Initialize subsystems
     initilialize_logging();
+    input_initialize();
 
     // TODO: Remove this
     KFATAL("A test message: %f", 3.14f);
@@ -98,6 +100,12 @@ b8 application_run()
                 app_state.is_running = FALSE;
                 break;
             }
+
+            // NOTE: Input update/state copying should always be handled
+            // after any input should be recorded; I.E. before this line.
+            // As a safety, input is the last thing to be updated before
+            // this frame ends
+            input_update(0);
         }
     }
 
@@ -105,6 +113,7 @@ b8 application_run()
     app_state.is_running = FALSE;
 
     event_shutdown();
+    input_shutdown();
 
     platform_shutdown(&app_state.platform);
 
